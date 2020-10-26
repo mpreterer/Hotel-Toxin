d1649172b4f11cfb7b6396968390e7cbf3b1eae6
Roland <mpreter@mail.ru>
2020-10-27 00:15:16 +0300

    Created counter

---
+++ click.js
@@ -0,0 +1,24 @@
+  const drop_block_row = function () {
+  const btns = document.querySelectorAll('.circle_drop_item');
+
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', function () {
+      const direction = this.dataset.direction;
+      const inp = this.parentElement.querySelector('.counter');
+      const currentValue = +inp.value;
+      let newValue;
+
+      if (direction === 'plus') {
+        newValue = currentValue + 1;
+      } else {
+        newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
+      }
+
+      inp.value = newValue;
+    })
+  })
+
+}
+
+drop_block_row();
