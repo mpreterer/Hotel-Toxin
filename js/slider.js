a21a340e6c4fe68c6b6236012958c2af27da9ce3
Roland <mpreter@mail.ru>
2020-11-05 01:09:10 +0300

    Finished blocks of rooms

--- js/slider.js
+++ js/slider.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         pips: {
             mode: 'values',
-             values: [0,5000, ,10000,11000,12000,13000,14000,15000],
+             values: [0,5000,15000],
              // density: 4
         }
     });
