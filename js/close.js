3482b1cbace5c8ffbcfd82894b137b002dc63dcc
Roland <mpreter@mail.ru>
2020-12-16 12:52:30 +0300

    Changed open block with js

--- js/close.js
+++ js/close.js
@@ -1,11 +1,27 @@
 const guest = document.querySelector('.guests')
-const drop = document.querySelector('.link_drop_function')
+const drop = document.getElementById('apply')
 const dropBlock = document.querySelector('.dropBlock')
+const block4 = document.querySelector('.block4')
+
 
 drop.onclick = () => {
-	if(drop.style.display === 'block') {
-		dropBlock.style.display = 'none'
-	} else {
-		dropBlock.style.display = 'block'
+	if(dropBlock.classList.contains('hide') ) {
+		dropBlock.classList.remove('hide')
+		block4.classList.add('hide')
+	}
+	else {
+		dropBlock.classList.add('hide')
+		block4.classList.remove('hide')
+	}
+}
+
+guest.onclick = () => {
+	if(dropBlock.classList.contains('hide') ) {
+		dropBlock.classList.toggle('hide')
+		block4.classList.add('hide')
+	}
+	else {
+		dropBlock.classList.toggle('hide')
+		block4.classList.remove('hide')
 	}
 }
