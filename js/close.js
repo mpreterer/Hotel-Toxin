b3bb313ca1e1e87ba75a64c4b97fd08907b3cbfc
Roland <mpreter@mail.ru>
2021-03-13 19:47:50 +0300

    Upgrade calendare on js

--- js/close.js
+++ js/close.js
@@ -1,27 +1,30 @@
-const guest = document.querySelector('.guests')
-const drop = document.getElementById('apply')
-const dropBlock = document.querySelector('.dropBlock')
-const block4 = document.querySelector('.block4')
+//block Guests
+const guest = document.querySelector('.guests');
+const drop = document.getElementById('apply');
+const dropBlock = document.querySelector('.dropBlock');
+const block4 = document.querySelector('.block4');
 
 
 drop.onclick = () => {
 	if(dropBlock.classList.contains('hide') ) {
-		dropBlock.classList.remove('hide')
-		block4.classList.add('hide')
+		dropBlock.classList.remove('hide');
+		block4.classList.add('hide');
 	}
 	else {
-		dropBlock.classList.add('hide')
-		block4.classList.remove('hide')
+		dropBlock.classList.add('hide');
+		block4.classList.remove('hide');
 	}
 }
 
 guest.onclick = () => {
 	if(dropBlock.classList.contains('hide') ) {
-		dropBlock.classList.toggle('hide')
-		block4.classList.add('hide')
+		dropBlock.classList.toggle('hide');
+		block4.classList.add('hide');
 	}
 	else {
-		dropBlock.classList.toggle('hide')
-		block4.classList.remove('hide')
+		dropBlock.classList.toggle('hide');
+		block4.classList.remove('hide');
 	}
 }
+
+
