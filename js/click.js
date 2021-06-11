beaac5fc23a6e7980627f01a8f376a4ac9a42679
Roland <mpreter@mail.ru>
2021-06-11 08:44:59 +0300

    Added count older

--- js/click.js
+++ js/click.js
@@ -1,3 +1,10 @@
+  const conGuests = document.querySelector('.counte_guests');
+  const c1 = document.querySelector('#counter1');
+
+  var old = 0;
+  var young = 0;
+  var both = 0;
+
   const drop_block_row = function () {
   const btns = document.querySelectorAll('.circle_drop_item');
 
@@ -10,11 +17,26 @@
 
       if (direction === 'plus') {
         newValue = currentValue + 1;
+
       } else {
         newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
+      } inp.value = newValue;
+
+
+      // Для взрослых
+      if (c1.value > 0 && direction === 'plus') {
+        conGuests.value = '';
+        old += 1;
+        conGuests.value += `${old} взрослый`;
+      } else if (c1.value <= 0) {
+        conGuests.value = '';
+        old = 0;
+      } else {
+        conGuests.value = '';
+        old -= 1;
+        conGuests.value += `${old} взрослый`;
       }
 
-      inp.value = newValue;
     })
   })
 
