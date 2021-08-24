a31a07fdcea37444d34a241b925ef67d38c46b16
Roland <mpreter@mail.ru>
2021-08-24 16:04:28 +0300

    Added possibility clear value from counter

--- js/clear.js
+++ js/clear.js
@@ -1,21 +1,33 @@
-  const drop_function = function () {
-  const btns = document.querySelectorAll('.link_drop_function');
+const clear = document.querySelector('#clear');
+const counterClear = document.querySelector('.counte_guests');
+const gst = document.querySelector('.guests');
+const dropB = document.querySelector('.dropBlock');
 
+var coun1 = document.querySelector('#counter1');
+var coun2 = document.querySelector('#counter2');
+var coun3 = document.querySelector('#counter3');
 
-  btns.forEach(btn => {
-    btn.addEventListener('click', function () {
-      const direction = this.dataset.direction;
-      const inp = this.parentElement.querySelector('.counter');
-      const currentValue = +inp.value;
-      let newValue;
+gst.addEventListener('click',function() {
+  if (counterClear.value === '') {
+    clear.classList.add('hiden_clear');
+  } else {
+    clear.classList.remove('hiden_clear');
+  }
+})
 
-      if (direction === 'clear') {
-        newValue = currentValue - 100;
-      }
-      inp.value = newValue;
-    })
-  })
+dropB.addEventListener('click', function() {
+  if (counterClear.value === '') {
+    clear.classList.add('hiden_clear');
+  } else {
+    clear.classList.remove('hiden_clear');
 
-}
+      clear.addEventListener('click',function() {
+        coun1.value = 0;
+        coun2.value = 0;
+        coun3.value = 0;
+        counterClear.value = '';
 
-drop_block_row();
+        clear.classList.add('hiden_clear');
+      })
+  }
+})
