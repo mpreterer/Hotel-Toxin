938b4e76980835b05eadd5d5dcad62c0ffc13d17
Roland <mpreter@mail.ru>
2021-08-26 11:18:30 +0300

    Create slider for block rooms

--- js/jquery.slider.js
+++ js/jquery.slider.js
@@ -1,14 +1,41 @@
-$(document).ready(function() {
+const ONE = 1;
+var numForCount = 1;
+
+$(document).ready(function(stopNumber) {
     $('.room_in_hotel').click(function(event){
         let tarName = event.currentTarget.className;
-        let rightClick = `${'.'+tarName.slice(0, tarName.indexOf(' ')) + ' .arrow_right'}`;
-        let leftClick = `${'.'+tarName.slice(0, tarName.indexOf(' ')) + ' .arrow_left'}`;
-        
-        if (event.target.className == 'move_arrow arrow_right') {
-            console.log($(`${rightClick}`))
+        let targetImage = `${'.'+tarName.slice(0, tarName.indexOf(' ')) + ' .container'}`;
+        let images = `${'.'+tarName.slice(0, tarName.indexOf(' ')) + ' .container' + ' .images-for-room'}`;
+
+        // Достаем путь до картинки
+        let G = $(`${targetImage}`).css('background-image');
+        // оставить чисто путь
+        let G1 = G.replace('url(','').replace(')','').replace(/\"/gi, "");
+        // оставить только само изображние
+        let G2 = G1.split('image')[1].substr(1);
+        // вырезать число для подсчета
+        let i = parseInt(G2.split('_')[0]);
+
+        if (event.target.classList.contains('arrow_right')) {
+            if (numForCount === 4) {
+                // вернуть изначальную юкратинку
+                i -= 3;
+                $(`${targetImage}`).css('background', `url(${'../image/' + i + '_room' + '.png'})`)
+                numForCount = 1;
+                console.log(numForCount)
+                
+            } else {
+                i += ONE;
+                $(`${targetImage}`).css('background', `url(${'../image/' + i + '_room' + '.png'})`)
+                numForCount += 1;
+
+                console.log(numForCount)
+                console.log(i)
+            }
         }
-        else if (event.target.className = 'move_arrow arrow_left') {
-            console.log($(`${leftClick}`))
+        else if (event.target.classList.contains('arrow_left')) {
+            i -= 1;
+            $(`${targetImage}`).css('background', `url(${'../image/' + i + '_room' + '.png'})`)
         }
     })
 })
