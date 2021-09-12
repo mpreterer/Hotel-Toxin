9fa1016072925a0a7b67a61792b15795937aabee
Roland <mpreter@mail.ru>
2021-09-12 14:31:43 +0300

    commit
    test

--- js/script-datepicker.js
+++
@@ -1,9 +0,0 @@
-// $('#minMaxExample').datepicker({
-//     // Можно выбрать тольо даты, идущие за сегодняшним днем, включая сегодня
-//     minDate: new Date()
-// })
-
-
-$('.datepicker-here').click(function(){
-    $('.datepicker .-bottom-left- .-from-bottom- .active').css('left','211px')
-})
