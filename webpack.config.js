abf3f7188710553a29ad96983fa4474d311205ad
mpreter <dshabelyan@mail.ru>
2021-09-27 23:55:01 +0300

    transfered files

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,13 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
+
+
+module.exports = () => {
+   
+    return {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
@@ -16,6 +21,11 @@ module.exports = {
         ignored: /node_modules/,
     },
     mode: 'development',
-
-    
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './pages/website-pages/index.pug',
+            filename: './index.html'
+        })
+    ]
+  }
 }
