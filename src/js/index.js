fff51d1d85be278531390b3147f9ed5dc523d552
mpreter <dshabelyan@mail.ru>
2021-10-14 10:16:24 +0300

    start rewrite code of blocks

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,23 @@
-import '../pages/style.scss'
-import '../pages/index.pug'
-import '../assets/sass/datepicker.min.scss'
-import './jquery-3.6.0.min.js'
-import './click.js'
-import './clear.js'
-import './close.js'
-import './datepicker.min.js'
-// import './date-field.js'
+// import '../pages/style.scss'
+// import '../pages/index.pug'
+// import '../assets/sass/datepicker.min.scss'
+// import './jquery-3.6.0.min.js'
+// import './click.js'
+// import './clear.js'
+// import './close.js'
+// import './datepicker.min.js'
 
-
-// import '../pages/website-pages/search-room/searchRoom.scss'
-// import '../pages/website-pages/login/login.scss'
-// import '../pages/website-pages/registration/Registration.scss'
-// import '../pages/website-pages/room-details/roomDetails.scss'
-
-
-// function importAll(resolve) {
-//     resolve.keys().forEach(resolve);
-//   }
+function importAll(resolve) {
+    resolve.keys().forEach(resolve);
+  }
   
-//   importAll(require.context('../assets/image', true, /\.(jpg|png|svg|png)$/));
-//   importAll(require.context('../assets/sass', true, /\.(css|scss)$/));
-//   importAll(require.context('../pages', true, /\.(css|scss)$/));
-//   importAll(require.context('./', true, /\.(js)$/));
+  
+  importAll(require.context('../image', true, /\.(jpg|png|svg|png)$/));
+//   importAll(require.context('../static/favicons/', true, /\.(svg|png|ico|xml|json)$/));
+  
+  importAll(require.context('../styles', true, /\.(css|scss)$/));
+  importAll(require.context('../blocks', true, /\.(css|scss|jpg|png|svg|png|ico|)$/));
+  importAll(require.context('../pages', true, /\.(css|scss)$/));
+  
+  importAll(require.context('./', true, /\.(js)$/));
+  importAll(require.context('../blocks', true, /\.(js)$/));
