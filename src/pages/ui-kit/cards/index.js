e26d35d83c011bd91e61af13529ec29d8007838f
mpreter <dshabelyan@mail.ru>
2022-03-21 17:32:27 +0300

    Change struct pug

---
+++ src/pages/ui-kit/cards/index.js
@@ -0,0 +1,12 @@
+import './cards.sass'
+import '../../style.scss'
+import '../../website-pages/search-room/searchRoom.scss'
+import '../../website-pages/registration/Registration.scss'
+import '../../website-pages/login/login.scss'
+import '../../website-pages/room-details/roomDetails.scss'
+import '../../../assets/scss/datepicker.min.scss'
+import '../../../assets/scss/range.scss'
+
+import '../../../js/click.js'
+import '../../../js/close.js'
+import '../../../js/datepicker.min.js' 
