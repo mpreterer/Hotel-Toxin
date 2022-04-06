417a86a4c5058b60ceb9fff4288e182603fcd87e
mpreter <dshabelyan@mail.ru>
2022-04-06 19:51:03 +0300

    Fix use heart

--- src/js/like.js
+++ src/js/like.js
@@ -1,18 +1,11 @@
 
-const like = document.querySelector('.fa_heart')
-const like2 = document.querySelector('.fa_heart2')
-const numb_like = document.querySelector('#span_heart')
-const heart = document.querySelector('.heart')
-const heart2 = document.querySelector('.heart_2')
-
-
-heart.addEventListener('click', ({ target: t }) => {
+document.addEventListener('click', ({ target: t }) => {
 	if (t.classList.contains('heart')) {
 	  const index = [...document.querySelectorAll('.heart')].indexOf(t);
 	  const count = document.querySelectorAll('.span_heart')[index];
 	  const heart = document.querySelectorAll('.fa_heart')[index];
-	  count.classList.toggle('active');
-	  heart.classList.toggle('fill_heart');
-	  count.innerText -= [ 1, -1 ][+count.classList.contains('active')];
+	  count.classList.toggle('span-heart_active');
+	  heart.classList.toggle('fill_heart')
+	  count.innerText -= [ 1, -1 ][+count.classList.contains('span-heart_active')];
 	}
 });
