f58c5694edb73de7d71bc3bffc971ca7266a3276
mpreter <dshabelyan@mail.ru>
2022-04-20 12:02:14 +0300

    added doughnut and correct calendar

--- src/pages/website-pages/room-details/index.js
+++ src/pages/website-pages/room-details/index.js
@@ -1,6 +1,8 @@
 import './roomDetails.scss'
 import '../../../assets/scss/datepicker.min.scss'
+import '../../../assets/scss/Chart.min.css'
 import '../../style.scss'
+
 
 import '../../../js/jquery-3.6.0.min.js'
 import '../../../js/datepicker.min.js' 
@@ -8,6 +10,40 @@ import '../../../js/like.js'
 import '../../../js/close.js'
 import '../../../js/clear.js'
 import '../../../js/click.js'
-import '../../../js/Chart.js'
+// import '../../../js/Chart.js'
 import '../../../js/scriptChart.js'
 import '../../../js/datepicker.min.js'
+import '../../../js/Chart.min.js'
+// const Chart = require('chart.js');
+
+let ctx = document.getElementById('myChart');
+ctx.height = 120;
+ctx.width = 120;
+
+const dataDoughnut = {
+    labels: ['Удовлетворительно','Хорошо','Великолепно'],
+    datasets: [
+        {
+            backgroundColor: ['#93A3FA','#69D1CF','#FFCA9C'],
+            data: [260,260,520],
+        }
+    ]
+}
+
+const chart = new Chart(ctx, {
+    type: 'doughnut',   
+    data: dataDoughnut,
+    options: {
+        legend: {
+            display: false,
+        },
+        cutoutPercentage: 90,
+    },
+});
+
+$('#first_date2').datepicker({ 
+    onSelect: function (fd, d, picker) { 
+      $("#first_date2").val(fd.split("-")[0]);
+      $("#second_date2").val(fd.split("-")[1]);
+    }
+});
