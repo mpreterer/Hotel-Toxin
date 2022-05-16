48b8ad64f85c9adc036fc8609dcefe855d9569cf
mpreter <dshabelyan@mail.ru>
2022-05-16 23:11:16 +0300

    Refactoring dropDown

--- src/blocks/dropDown/dropDown.js
+++ src/blocks/dropDown/dropDown.js
@@ -1,3 +1,4 @@
+const DROP_DOWN_TARGET = document.querySelector('.dropDown');
 const counter = document.querySelector('.dropDown__counte-guests');
 
 const c1 = document.querySelector('#counter1');
@@ -20,41 +21,102 @@ var young = 0;
 var strGuest = '';
 var strBoth = '';
 
-function plus(directPlus) {
-  directPlus.addEventListener('click', () => {
-    old += 1;
+var topStringFirst = 'гость';
+var topStringSecond = 'гостя';
+var topStringThreeth = 'гостей';
+
+
+$(document).ready(function() {  
+
+  $('.dropDown').click(function(event){
+
+  let counterString = event.currentTarget.querySelector('.dropDown__counte-guests');
+
+  let counterInput = event.currentTarget.querySelector('#counter1');
+  let counterInput2 = event.currentTarget.querySelector('#counter2');
+  let counterInput3 = event.currentTarget.querySelector('#counter3');
+
+  let attrItem1 = event.currentTarget.querySelector('.dropBlock__block-input1');
+  let attrItem2 = event.currentTarget.querySelector('.dropBlock__block-input2');
+  let attrItem3 = event.currentTarget.querySelector('.dropBlock__block-input3');
+
+  let plusAttr = attrItem1.getAttribute('count');
+  let plusAttrYoung = attrItem2.getAttribute('count');
+
+
+  if (event.target.className == plus1.className) {
+    
+      attrItem1.setAttribute('count',`${++plusAttr}`);
+      console.log(plusAttr)
+      
+      if ( both === 0 ) {
+        if ( plusAttr + plusAttrYoung === 1 || plusAttr + plusAttrYoung === 21 )
+          {
+            counterString.value = `${plusAttr} ${topStringFirst}`;
+        } else if ( plusAttr + plusAttrYoung >= 2 && plusAttr + plusAttrYoung <= 4 || plusAttr + plusAttrYoung >= 22 && plusAttr + plusAttrYoung <= 24) {
+            counterString.value= `${plusAttr} ${topStringSecond}`;
+        } else {
+            counterString.value = `${plusAttr} ${topStringThreeth}`;
+        }
+  
+      } else {
+        if ( plusAttr + plusAttrYoung === 1 || plusAttr + plusAttrYoung === 21 )
+          {
+            strGuest = topStringFirst;
+            currentCounter.value = `${plusAttr + young} ${strGuest} ${both} ${strBoth}`;
+        } else if ( plusAttr + plusAttrYoung >= 2 && plusAttr + plusAttrYoung <= 4 || plusAttr + plusAttrYoung >= 22 && plusAttr + plusAttrYoung <= 24) {
+            strGuest = topStringSecond;
+            currentCounter.value = `${plusAttr + young} ${strGuest} ${both} ${strBoth}`;
+        } else {
+            strGuest = topStringThreeth;
+            currentCounter.value = `${plusAttr + young} ${strGuest} ${both} ${strBoth}`;
+        }
+  
+      }
+      
+      counterInput.value = plusAttr;
+  }
+
+  if (event.target.className == plus2.className) {
+    
+    attrItem2.setAttribute('count',`${++plusAttr}`);
+    
+    let counterTemp = counterString.value.replace(/[^\d]/g, '');
+
+    counterTemp++;
 
     if ( both === 0 ) {
-      if ( old + young === 1 || old + young === 21 )
+      if ( attrItem1 + young === 1 || attrItem1 + young === 21 )
         {
-          strGuest = 'гость';
-          counter.value = `${old + young} ${strGuest}`;
-      } else if ( old + young >= 2 && old + young <= 4 || old + young >= 22 && old + young <= 24) {
-          strGuest = 'гостя'
-          counter.value = `${old + young} ${strGuest}`;
+          counterString.value = `${counterTemp} ${topStringFirst}`;
+      } else if ( counterTemp + plusAttrYoung >= 2 && counterTemp + plusAttrYoung <= 4 || counterTemp + plusAttrYoung >= 22 && counterTemp + plusAttrYoung <= 24) {
+          counterString.value= `${counterTemp} ${topStringSecond}`;
       } else {
-          strGuest = 'гостей';
-          counter.value = `${old + young} ${strGuest}`;
+          counterString.value = `${counterTemp} ${topStringThreeth}`;
       }
 
     } else {
-      if ( old + young === 1 || old + young === 21 )
+      if ( attrItem1 + young === 1 || attrItem1 + young === 21 )
         {
-          strGuest = 'гость';
-          counter.value = `${old + young} ${strGuest} ${both} ${strBoth}`;
-      } else if ( old + young >= 2 && old + young <= 4 || old + young >= 22 && old + young <= 24) {
-          strGuest = 'гостя'
-          counter.value = `${old + young} ${strGuest} ${both} ${strBoth}`;
+          strGuest = topStringFirst;
+          currentCounter.value = `${counterTemp + young} ${strGuest} ${both} ${strBoth}`;
+      } else if ( attrItem1 + young >= 2 && attrItem1 + young <= 4 || attrItem1 + young >= 22 && attrItem1 + young <= 24) {
+          strGuest = topStringSecond;
+          currentCounter.value = `${counterTemp + young} ${strGuest} ${both} ${strBoth}`;
       } else {
-          strGuest = 'гостей';
-          counter.value = `${old + young} ${strGuest} ${both} ${strBoth}`;
+          strGuest = topStringThreeth;
+          currentCounter.value = `${counterTemp + young} ${strGuest} ${both} ${strBoth}`;
       }
 
     }
-
-    c1.value = old;
-  })
+    
+    counterInput.value = counterTemp;
 }
+
+  })
+})
+
+  
 
 function minus(directMinus) {
   directMinus.addEventListener('click', () => {
@@ -273,7 +335,6 @@ clear.onclick = function() {
   strBoth = '';
 }
 
-plus(plus1);
 minus(minus1);
 
 plusYoung(plus2);
