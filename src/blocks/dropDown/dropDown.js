e4c4618c896f051e202839f921bce32be3cfb9af
mpreter <dshabelyan@mail.ru>
2022-05-19 10:11:22 +0300

    Rewrtire dropDown to class

--- src/blocks/dropDown/dropDown.js
+++ src/blocks/dropDown/dropDown.js
@@ -1,31 +1,67 @@
 class dropDown {
-    constructor(domParent, keyWord) {
-        this.container = domParent.querySelectore('.js-dropdown');
-        this.keyWord = keyWord;
+    constructor(domParemt) {
+        this.container = domParemt;
 
-        this._createDomElements();
         this._init();
     }
 
     _init() {
-        
-    }
-
-    _createDomElements() {
-        this.inputName = this.container.querySelectore('.js-dropDown__counte-guests');
-        this.isOpen = this.container.hasAttribute('data-is-open');
-        this.counterName = this.container.querySelectoreAll('[data-type="name-counter"]');
-        this.counter = this.container.querySelectoreAll('[data-type="counter-value"]');
-        this.listRow = this.container.querySelectore('.js-dropDown__row');
-        this.btnPlus = this.container.querySelectoreAll('[data-button="circle-plus"]');
-        this.btnMinus = this.container.querySelectoreAll('[data-button="circle-minus"]');
-        this.controlPanel = this.container.querySelectore('.dropDown__function');
+        this.inputName = this.container.querySelector('.js-dropDown__counte-guests');
+        this.open = this.container.querySelector('.js-dropDown__name');
+        this.counterPanel = this.container.querySelector('.js-dropDown__drop-block');
+        this.counterContext = this.container.querySelector('.js-dropDown__items');
+        this.counterName = this.container.querySelectorAll('.dropDown__dropItem');
+        this.counter = this.container.querySelectorAll('.dropDown__counter');
+        this.btnPlus = this.container.querySelectorAll('.dropDown__circle-plus');
+        this.btnMinus = this.container.querySelectorAll('.dropDown__circle-minus');
+        this.controlPanel = this.container.querySelector('.dropDown__function');
         this.hasControlPanel = this.controlPanel !== null && this.controlPanel !== undefined;
 
         if (this.hasControlPanel) {
-            this.btnApply = this.container.querySelectore('[data-button="apply"]');
-            this.btnClear = this.container.querySelectore('[data-button="clear"]');
+            this.btnApply = this.container.querySelector('.dropDown__function-apply');
+            this.btnClear = this.container.querySelector('.dropDown__function-clear');
+        }
+        
+        this._open_drop_down();
+        this._drop_down_counter();
+    }
+
+    _open_drop_down() {
+        this.open.addEventListener('click', () => {
+            if (this.counterPanel.classList.contains('js-drop-close')) {
+                this.counterPanel.classList.toggle('js-drop-close');
+                this.counterPanel.classList.add('js-drop-open');
+            } else {
+                this.counterPanel.classList.toggle('js-drop-open');
+                this.counterPanel.classList.add('js-drop-close');
+            }
+        })
+    }
+
+    _drop_down_counter() {
+        this.counterContext.addEventListener('click', this._drop_down_counter_handler.bind(this));
+    }
+
+    _drop_down_counter_handler(event) {
+        let target = event.target;
+
+        if(target.classList.contains('js-dropDown-plus')) {
+            this._button_plus(target);
+            console.log(target)
+        } else if(target.classList.contains('js-dropDown-minus')) {
+            this._button_minus(target);
         }
     }
+
+    _button_plus(element) {
+        console.log(element.previousElementSibling);
+    }
+
+    _button_minus(element) {
+        console.log(element.previousElementSibling);
+    }
+
+
 }
 
+export default dropDown;
--- src/blocks/dropDown/dropDownInit.js
+++ src/blocks/dropDown/dropDownInit.js
@@ -1,9 +1,9 @@
 import dropDown from './dropDown.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const dropDowns = document.querySelectorAll('.js-dropdown');
-    
-    dropDowns.forEach(element => {
-        const dropdown = new dropDown(element);
-    });
+    const dropDowns = document.querySelectorAll('.js-dropDown');
+
+    dropDowns.forEach(domParent => {
+        const dropdown = new dropDown(domParent);
+    })
 });
