7f2f6169c5b1bdd6786b88fe076dbe0c23bbd724
mpreter <dshabelyan@mail.ru>
2022-06-13 13:24:24 +0300

    Now images don't repeat

--- webpack.config.js
+++ webpack.config.js
@@ -10,186 +10,159 @@ const isProd = !isDev;
 
 const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
-module.exports = {
-    mode: 'development',
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        index: ['./pages/index.js'],
-        'search-room': ['./pages/website-pages/search-room/index.js'],
-        'room_details': ['./pages/website-pages/room-details/index.js'],
-        'login': ['./pages/website-pages/login/index.js'],
-        'registration': ['./pages/website-pages/registration/index.js'],
-        'site-pages': ['./pages/ui-kit/index.js'],
-        'cards': ['./pages/ui-kit/cards/index.js'],
-        'colors-type': ['./pages/ui-kit/colors-type/index.js'],
-        'elements': ['./pages/ui-kit/elements/index.js'],
-        'header-footer': ['./pages/ui-kit/header-footer/index.js'],
-    },
-    output: {
-        filename: 'js/[name].js',
-        path: path.resolve(__dirname,'dist')
-    },
-    watchOptions: {
-        ignored: /node_modules/,
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
-        open: true,
-        historyApiFallback: true,
-        compress: true,
-        hot: true,
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                from: path.resolve(__dirname, 'src/assets'),
-                to: path.resolve(__dirname, 'dist/assets'),
-                },
-            ],
-            }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash].css',
-        }),
-        new HTMLWebpackPlugin({
-            template: './pages/index.pug',
-            filename: './index.html',
-            chunks: ['index']
-        }),
-        new HTMLWebpackPlugin({
-            template: './pages/website-pages/search-room/searchRoom.pug',
-            filename: './searchRoom.html',
-            chunks: ['search-room']
-        }),
-        new HTMLWebpackPlugin({
-            template: './pages/website-pages/room-details/roomDetails.pug',
-            filename: './roomDetails.html',
-            chunks: ['room_details']
-        }),
-        new HTMLWebpackPlugin({
-            template: './pages/website-pages/login/login.pug',
-            filename: './login.html',
-            chunks: ['login']
-        }),
-        new HTMLWebpackPlugin({
-            template: './pages/website-pages/registration/Registration.pug',
-            filename: './Registration.html',
-            chunks: ['registration']
-        }),
-        new HTMLWebpackPlugin({
-            template: './pages/ui-kit/site-pages.pug',
-            filename: './ui-kit/site-pages.html',
-            chunks: ['site-pages']
-        }),
-        new HTMLWebpackPlugin({
-            template: './pages/ui-kit/cards/cards.pug',
-            filename: './ui-kit/cards.html',
-            chunks: ['cards']
-        }),
-        new HTMLWebpackPlugin({
-            template: './pages/ui-kit/colors-type/colors-type.pug',
-            filename: './ui-kit/colors-type.html',
-            chunks: ['colors-type']
-        }),
-        new HTMLWebpackPlugin({
-            template: './pages/ui-kit/elements/elements.pug',
-            filename: './ui-kit/elements.html',
-            chunks: ['elements']
-        }),
-        new HTMLWebpackPlugin({
-            template: './pages/ui-kit/header-footer/header-footer.pug',
-            filename: './ui-kit/header-footer.html',
-            chunks: ['header-footer']
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: ('./pages/[name].[contenthash].css')
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, './src/assets'),
-                    to:   path.resolve(__dirname, './dist/assets')
-                }
-                ]
-            }),
-        new MiniCssExtractPlugin({
-            filename: './css/[name].[hash].css',
-        })
-    ],
-    module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env'],
-              },
-            },
-          },
-          {
-            test: /\.(sa|sc|c)ss$/,
-            use: [
-              {
-                loader: MiniCssExtractPlugin.loader,
-                options: {
-                  publicPath: (resourcePath, context) => {
-                    return path.relative(path.dirname(resourcePath), context) + '/';
+module.exports = () => {
+
+    const PATHS = {
+        src: path.join(__dirname, './src'),
+        dist: path.join(__dirname, './dist'),
+    };
+    
+    const PAGES_DIR = `${PATHS.src}/pages/`;
+
+    return {
+        mode: 'development',
+        entry: {
+            index: [`${PAGES_DIR}/index.js`],
+            'search-room': [`${PAGES_DIR}/website-pages/search-room/index.js`],
+            'room_details': [`${PAGES_DIR}/website-pages/room-details/index.js`],
+            'login': [`${PAGES_DIR}/website-pages/login/index.js`],
+            'registration': [`${PAGES_DIR}/website-pages/registration/index.js`],
+            'site-pages': [`${PAGES_DIR}/ui-kit/index.js`],
+            'cards': [`${PAGES_DIR}/ui-kit/cards/index.js`],
+            'colors-type': [`${PAGES_DIR}/ui-kit/colors-type/index.js`],
+            'elements': [`${PAGES_DIR}/ui-kit/elements/index.js`],
+            'header-footer': [`${PAGES_DIR}/ui-kit/header-footer/index.js`],
+        },
+        output: {
+            filename: 'js/[name].js',
+            path: path.resolve(__dirname,'dist'),
+            assetModuleFilename: 'assets/images/[name][ext]'
+        },
+        watchOptions: {
+            ignored: /node_modules/,
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery',
+              }),
+            new CleanWebpackPlugin(),
+            new CopyWebpackPlugin({
+                patterns: [
+                  {
+                    from: path.resolve(__dirname, 'src/assets'),
+                    to: path.resolve(__dirname, 'dist/assets'),
                   },
-                },
-              },
-              {
-                loader: 'css-loader',
-              },
-              {
-                loader: 'sass-loader',
-              },
-              {
-                loader: 'sass-resources-loader',
-                options: {
-                  resources: path.resolve(__dirname, 'src/assets/styles/_vars.scss'),
-                },
-              },
-            ],
-          },
-          {
-            test: /\.pug$/,
-            use: {
-              loader: 'pug-loader',
-            },
-          },
-          {
-            test: /\.(gif|png|jpe?g|svg|ico)$/,
-            exclude: [/fonts/],
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: 'assets/image/[name].[ext]',
-                  publicPath: '../',
-                },
-              },
-            ],
-          },
-          {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            exclude: [/image/],
-            use: {
-              loader: 'file-loader',
-              options: {
-                name: 'assets/fonts/[name].[ext]',
-                publicPath: '../',
-              },
-            },
-          },
+                ],
+              }),
+            new HTMLWebpackPlugin({
+                template: `${PAGES_DIR}/index.pug`,
+                filename: './index.html',
+                chunks: ['index']
+            }),
+            new HTMLWebpackPlugin({
+                template: `${PAGES_DIR}/website-pages/search-room/searchRoom.pug`,
+                filename: './searchRoom.html',
+                chunks: ['search-room']
+            }),
+            new HTMLWebpackPlugin({
+                template: `${PAGES_DIR}/website-pages/room-details/roomDetails.pug`,
+                filename: './roomDetails.html',
+                chunks: ['room_details']
+            }),
+            new HTMLWebpackPlugin({
+                template: `${PAGES_DIR}/website-pages/login/login.pug`,
+                filename: './login.html',
+                chunks: ['login']
+            }),
+            new HTMLWebpackPlugin({
+                template: `${PAGES_DIR}/website-pages/registration/Registration.pug`,
+                filename: './Registration.html',
+                chunks: ['registration']
+            }),
+            new HTMLWebpackPlugin({
+                template: `${PAGES_DIR}/ui-kit/site-pages.pug`,
+                filename: './ui-kit/site-pages.html',
+                chunks: ['site-pages']
+            }),
+            new HTMLWebpackPlugin({
+                template: `${PAGES_DIR}/ui-kit/cards/cards.pug`,
+                filename: './ui-kit/cards.html',
+                chunks: ['cards']
+            }),
+            new HTMLWebpackPlugin({
+                template: `${PAGES_DIR}/ui-kit/colors-type/colors-type.pug`,
+                filename: './ui-kit/colors-type.html',
+                chunks: ['colors-type']
+            }),
+            new HTMLWebpackPlugin({
+                template: `${PAGES_DIR}/ui-kit/elements/elements.pug`,
+                filename: './ui-kit/elements.html',
+                chunks: ['elements']
+            }),
+            new HTMLWebpackPlugin({
+                template: `${PAGES_DIR}/ui-kit/header-footer/header-footer.pug`,
+                filename: './ui-kit/header-footer.html',
+                chunks: ['header-footer']
+            }),
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: ('./pages/[name].[contenthash].css')
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                      from: path.resolve(__dirname, './src/assets/image'),
+                      to:   path.resolve(__dirname, './dist/assets/image')
+                    }
+                  ]
+                }),
+            new MiniCssExtractPlugin({
+                filename: './css/[name].[hash].css',
+            })
         ],
-      },
+        stats: {
+            children: true
+        },
+        module: {
+            rules: [
+                {
+                    test:/\.html$/,
+                    use: ['html-loader']
+                },
+                {
+                    test:/\.css$/,
+                    use: ['css-loader'] 
+                },
+                {
+                    test:/\.sass$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader',
+                    ]
+                },
+                {
+                    test:/\.scss$/,  
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader',
+                    ]
+                },
+                {
+                    test:/\.(png|svg|jpg)$/i,
+                    type: 'asset/resource'
+                },
+                {
+                    test:/\.pug$/,
+                    use: ['pug-loader'],
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.js','.json','.sass']
+        },
+    }
 }
