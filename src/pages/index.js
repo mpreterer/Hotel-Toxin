166d8f6cee22bdee539845ba401c3d99a9ac5f5e
mpreter <dshabelyan@mail.ru>
2022-06-13 15:49:47 +0300

    Added desc for index pages, correct styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
 import './style.scss'
-import '../blocks/filter/filter.scss'
 import '../blocks/button/button.scss'
 import '../blocks/dropDown/dropDown.scss'
-import '../blocks/footer/footer.scss'
-import '../blocks/header/header.scss'
 import '../blocks/calendar/calendar.scss'
 import '../blocks/range-date-calendar/range-date-calendar.scss'
 
-
 import '../js/jquery-3.6.0.min.js'
-import '../blocks/dropDown/dropDown'
 import '../blocks/dropDown/dropDownInit.js'
 import '../blocks/dropDown/dropDown.js'
 import '../blocks/range-date-calendar/range-date-calendar-init.js'
