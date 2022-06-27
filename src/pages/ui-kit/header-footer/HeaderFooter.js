ac1e0ba109fa336ea9da310dc21a21564d100658
mpreter <dshabelyan@mail.ru>
2022-06-27 10:48:10 +0300

    Write class HeaderFooter

--- src/pages/ui-kit/header-footer/HeaderFooter.js
+++ src/pages/ui-kit/header-footer/HeaderFooter.js
@@ -2,3 +2,25 @@ import './headerFooter.scss';
 import '../../../blocks/header/header.scss';
 import '../../../blocks/footer/footer.scss';
 import '../../../pages/style.scss';
+import NavMenu from '../../../blocks/nav-menu/nav-menu';
+
+class HeaderFooter {
+    constructor() {
+        this._init();
+    }
+
+    _init() {
+        const { navMenu } = this._get_element();
+        navMenu.forEach((item) => new NavMenu(item));
+    }
+
+    _get_element() {
+        return {
+            navMenu: document.querySelectorAll('[data-component-js="js-header"]'),
+        };
+    }
+}
+
+export default HeaderFooter;
+
+new HeaderFooter();
