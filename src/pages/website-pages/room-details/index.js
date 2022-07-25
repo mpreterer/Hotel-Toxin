5734a2cfa5fb7ce4280d923fe1c77e30ccf9c1f3
mpreter <dshabelyan@mail.ru>
2022-07-25 11:25:11 +0300

    Delete not use import

--- src/pages/website-pages/room-details/index.js
+++ src/pages/website-pages/room-details/index.js
@@ -2,11 +2,10 @@ import './room-details.scss';
 import '../../style.scss';
 import '../../../assets/styles/reset.scss';
 
-import '../../../blocks/impressions-room';
-import '../../../blocks/button-like';
-import '../../../blocks/room-order';
-import '../../../blocks/user-reviews';
 import '../../../blocks/header';
 import '../../../blocks/footer';
+import '../../../blocks/impressions-room';
+import '../../../blocks/room-order';
+import '../../../blocks/user-reviews';
 import '../../../blocks/about-room';
 import '../../../blocks/room-orders';
