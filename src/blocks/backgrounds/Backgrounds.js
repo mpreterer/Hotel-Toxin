9ba60af31337269a709cb469d9d3f466e504eeda
mpreter <dshabelyan@mail.ru>
2022-09-15 09:18:07 +0300

    Fix counting length backgrounds, removed anonymous handler #11

--- src/blocks/backgrounds/Backgrounds.js
+++ src/blocks/backgrounds/Backgrounds.js
@@ -1,3 +1,5 @@
+import backgroundsClassNames from './utils/backgroundsClassNames';
+
 class Backgrounds {
   constructor(domParent) {
     this.container = domParent;
@@ -7,29 +9,34 @@ class Backgrounds {
   _init() {
     this.arrayBackgroundsJson = this.container.getAttribute('data-src');
     this._animateBackground();
+    window.onload = this._setTransition();
     this._setTransition();
   }
 
   _setTransition() {
-    window.onload = () => {
-      this.container.style.transition = '1s';
-      this.container.style.WebkitTransition = '1s';
-      this.container.style.MozTransition = '1s';
-    };
+    this.container.classList.add(backgroundsClassNames.transition);
   }
 
   _animateBackground() {
     const arrayBackgroundsParse = JSON.parse(this.arrayBackgroundsJson);
+    const lengthArrayBackgrounds = arrayBackgroundsParse.src.length;
     let counter = 0;
     
     this.container.style.backgroundImage = `url('${arrayBackgroundsParse.src[counter].background}')`;
     counter += 1;
 
     setInterval(() => {
-      if (counter === 3) {
+      this.container.classList.remove(backgroundsClassNames.transition);
+      this.container.classList.add(backgroundsClassNames.high_opacity);
+    }, 6000);
+
+    setInterval(() => {
+      if (counter === lengthArrayBackgrounds) {
         counter = 0;
       }
 
+      this.container.classList.remove(backgroundsClassNames.high_opacity);
+      this.container.classList.add(backgroundsClassNames.transition);
       this.container.style.backgroundImage = `url('${arrayBackgroundsParse.src[counter].background}')`;
 
       counter += 1;
---
+++ src/blocks/backgrounds/utils/backgroundsClassNames.js
@@ -0,0 +1,6 @@
+const backgroundsClassNames = {
+  transition: 'backgrounds_transitioned',
+  high_opacity: 'backgrounds_opacity_high',
+};
+
+export default backgroundsClassNames;
