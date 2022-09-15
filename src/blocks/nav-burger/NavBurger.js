667a0c097c85f79f8bfeb6aef0a3889c01497d71
mpreter <dshabelyan@mail.ru>
2022-09-15 15:41:01 +0300

    Added function view submenu for burger #2

--- src/blocks/nav-burger/NavBurger.js
+++ src/blocks/nav-burger/NavBurger.js
@@ -9,12 +9,41 @@ class NavBurger {
   _init() {
     this.body = document.body;
     this.menu = this.container.querySelector('.js-menu-burger');
+    this.items = this.container.querySelectorAll('.js-burger-item');
+    this.subList = this.container.querySelector('.js-burger-sub-list');
+    this.button = this.container.querySelector('.js-burger-button');
+
     this._initEvents();
   }
 
   _initEvents() {
-    this.container.addEventListener('click', this._handleNavBurgerClick.bind(this));
+    this.button.addEventListener('click', this._handleNavBurgerClick.bind(this));
+    this.items.forEach((el) => {
+      if (el.querySelector('.js-burger-sub-list')) {
+        el.addEventListener('click', this._handleItemClick.bind(this));
+      }
+    });
     this.body.addEventListener('click', this._handleBodyClick.bind(this));
+  }
+
+  _handleItemClick(event) {
+    if (event.currentTarget.classList.contains(navBurgerClassNames.ITEM_ACTIVE)) {
+      event.currentTarget
+        .classList.remove(navBurgerClassNames.ITEM_ACTIVE);
+      event.currentTarget.querySelector('.js-burger-sub-list')
+        .classList.remove(navBurgerClassNames.SUB_LIST_ACTIVE);
+      event.currentTarget
+        .querySelector('.js-burger-submenu')
+        .classList.remove(navBurgerClassNames.SUB_MENU_OPEN);
+    } else {
+      event.currentTarget
+        .classList.add(navBurgerClassNames.ITEM_ACTIVE);
+      event.currentTarget.querySelector('.js-burger-sub-list')
+        .classList.add(navBurgerClassNames.SUB_LIST_ACTIVE);
+      event.currentTarget
+        .querySelector('.js-burger-submenu')
+        .classList.add(navBurgerClassNames.SUB_MENU_OPEN);
+    }
   }
 
   _handleBodyClick(event) {
--- src/blocks/nav-burger/utils/navBurgerClassNames.js
+++ src/blocks/nav-burger/utils/navBurgerClassNames.js
@@ -1,5 +1,8 @@
 const navBurgerClassNames = {
   BURGER_OPEN: 'nav-burger__menu-burger_open',
+  SUB_MENU_OPEN: 'nav-burger__submenu_open',
+  SUB_LIST_ACTIVE: 'nav-burger__sub-list_open',
+  ITEM_ACTIVE: 'nav-burger__item_active',
 };
 
 export default navBurgerClassNames;
