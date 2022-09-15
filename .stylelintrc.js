7130cee363ff85ab038db6a5a4fed459f75924f2
mpreter <dshabelyan@mail.ru>
2022-09-15 11:15:35 +0300

    Correct style code

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    extends: [
-      "stylelint-config-standard",
-      "stylelint-config-rational-order",
-      "stylelint-prettier/recommended",
-    ],
-    plugins: [
-      "stylelint-order", 
-      "stylelint-scss"
-    ],
-    customSyntax: "postcss-scss",
-    rules: {
-      "prettier/prettier": true,
-      "at-rule-no-unknown": null,
-      "scss/at-rule-no-unknown": true,
-    },
-  }
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-rational-order",
+    "stylelint-prettier/recommended",
+  ],
+  plugins: [
+    "stylelint-order", 
+    "stylelint-scss"
+  ],
+  customSyntax: "postcss-scss",
+  rules: {
+    "prettier/prettier": true,
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+  },
+}
